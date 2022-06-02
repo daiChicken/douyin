ed426dd4a893321a1823a7540cda127453d4722d
jjychong <1065526695@qq.com>
2022-06-02 09:21:30 +0800

    新增err

--- controller/code.go
+++ controller/code.go
@@ -1,32 +1,37 @@
 package controller
 
+
 //定义一些可能出现的错误码
 
 type ResCode int64
-
-const (
-	CodeSuccess ResCode = 1000 + iota
+const(
+	CodeSuccess ResCode = 1000 +iota
 	CodeServerBusy
 	CodeNeedLogin
 	CodeInvalidToken
 	CodeSaveSuccess
-	CodeInvalidParam
+	CodeInvalidErr
+	CodeUserNotExist
+	CodeNotAccordStandard
 )
 
 var codeMsgMap = map[ResCode]string{
-	CodeSuccess:      "success",
-	CodeServerBusy:   "服务繁忙",
-	CodeNeedLogin:    "需要登录",
-	CodeInvalidToken: "无效Token",
-	CodeSaveSuccess:  "保存成功",
-	CodeInvalidParam: "请求参数有误",
+	CodeSuccess:				"success",
+	CodeServerBusy:				"服务繁忙",
+	CodeNeedLogin:				"需要登录",
+	CodeInvalidToken:			"无效Token",
+	CodeSaveSuccess:         	"保存成功",
+	CodeInvalidErr:				"参数错误",
+	CodeUserNotExist:			"用户不存在",
+	CodeNotAccordStandard: 		"必填字段未填或不符合数据规范",
 }
 
 // Msg 返回特定的错误提示信息
-func (c ResCode) Msg() string {
-	msg, ok := codeMsgMap[c]
-	if !ok {
+func (c ResCode)Msg()string{
+	msg,ok := codeMsgMap[c]
+	if !ok{
 		return codeMsgMap[CodeServerBusy]
 	}
 	return msg
 }
+
